140803954ed13ef39e27ef23c015e94fda644ba6
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-08-22 14:00:10 +0300

    lesson_8 Stack Queue

---
+++ algorithm/src/main/java/de/telran/lesson_8_20240822/SimpleStack.java
@@ -0,0 +1,25 @@
+package de.telran.lesson_8_20240822;
+
+public class SimpleStack {
+    public static void main(String[] args) {
+        ClassicStack stack = new ClassicStack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6); // не вставиться
+        stack.printStack();
+
+        System.out.println("take out - "+stack.pop());
+        stack.printStack();
+
+        System.out.println("take out - "+stack.pop());
+        stack.printStack();
+
+        System.out.println("Size = "+stack.getSize());
+
+        System.out.println("Top - "+stack.peek());
+        stack.printStack();
+    }
+}
